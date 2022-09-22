1657a5010ddd4543647ce81d8290b76e14ac19dd
Lex <matveychuk.m@gmail.com>
2022-09-22 21:24:09 +0300

    added HomeWorkApp and printThreeWords() and checkSumSign()

--- src/HomeWork4/HomeWorkApp.java
+++ src/HomeWork4/HomeWorkApp.java
@@ -4,15 +4,29 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
         printThreeWords();
+        checkSumSign();
 
     }
 
     public static void printThreeWords() {
-
+        System.out.println("1) printThreeWords:");
         System.out.println("""
                 Orange
                 Banana
                 Apple""");
+
+    }
+
+    public static void checkSumSign() {
+        System.out.println("2) checkSumSign:");
+        int a = 4;
+        int b = 8;
+
+        if ((a + b) >= 0) {
+            System.out.println("Sum is positive.");
+        } else {
+            System.out.println("Sum is negative.");
+        }
 
     }
 
