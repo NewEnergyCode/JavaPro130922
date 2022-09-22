690be5d8086a0166c526ff34463c50cfdfdc20af
Lex <matveychuk.m@gmail.com>
2022-09-22 21:13:08 +0300

    added HomeWorkApp and printThreeWords()

---
+++ src/HomeWork4/HomeWorkApp.java
@@ -0,0 +1,20 @@
+package HomeWork4;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+
+        printThreeWords();
+
+    }
+
+    public static void printThreeWords() {
+
+        System.out.println("""
+                Orange
+                Banana
+                Apple""");
+
+    }
+
+
+}
